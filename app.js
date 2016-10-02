4dc0d88befacb0416b98595d2ea44be24c2b1090
Robert van der Mark <robert.vandermark@gmail.com>
2016-10-02 12:09:36 -0400

    fixed nodemon

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ var app = express();
 
 module.exports = app;
 
+// app.use(express.static(path.join(__dirname, 'node_modules')));
+// app.use(express.static(path.join(__dirname, 'browser')));
+
+app.get('/', function(req, res, next){
+	res.send('Hello');
+});
+
